2b085013bfd197e4144d82d16be2316e177ed0dd
Ahad Iqbal <iqbalahad03@gmail.com>
2025-07-17 16:56:26 +0200

    test

--- src/ir_entrypoint.py
+++ src/ir_entrypoint.py
@@ -2,24 +2,23 @@ import os
 from src.search import boolean_retrieval
 from src.index.inverted_index import InvertedIndex
 
-# Globaler Index – wird nur einmal geladen
 INV = None
 
 def IR_main(index_path, query):
     """
     Parameters:
-        index_path (str): Pfad zu 'dict_final.tsv' und 'postings_final.bin'
-        query (str): Textbasierte Suchanfrage
+        index_path (str): path to 'dict_final.tsv' and 'postings_final.bin'
+        query (str): textbased query
 
     Returns:
-        list[int]: Dokument-IDs (sortiert aufsteigend, aber nicht nach Relevanz)
+        list[int]: Document IDs
     """
     global INV
     if INV is None:
         dict_file = os.path.join(index_path, 'dict_final.tsv')
         post_file = os.path.join(index_path, 'postings_final.bin')
         INV = InvertedIndex(dict_file, post_file)
-        boolean_retrieval.INV = INV  # Übergib geladenen Index an boolean_retrieval
+        boolean_retrieval.INV = INV 
 
-    return boolean_retrieval.main(query)  # verwendet deine bestehende Suchlogik
+    return boolean_retrieval.main(query) 
 
