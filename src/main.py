5b30552b94606b436ea7808a3fede8a84982351c
Ahad Iqbal <iqbalahad03@gmail.com>
2025-07-17 16:59:22 +0200

    updated main

--- src/main.py
+++ src/main.py
@@ -1,7 +1,6 @@
 import sys
 import os
 
-# Pfad zur Projektwurzel sicherstellen
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from src.ir_entrypoint import IR_main
